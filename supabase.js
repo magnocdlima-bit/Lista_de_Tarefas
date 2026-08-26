c90d9afda9d59abfe9897c9f68754cd6db3211f8
magno de lima <magnocdlima@gmail.comm>
2026-08-26 15:56:00 -0300

    atualização

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://gklrtjkxqyyxqdzrvwsa.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ydBjkRTx-CAIaNyQtSOVUg_afmYjPHd";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
